6bd1dc0660c19d536ee831734ec7ff1e0182ba45
YMRX <laurence.hupp@gmx.de>
2016-06-18 15:15:24 +0200

    Update imu.h

--- imu.h
+++ imu.h
@@ -17,6 +17,7 @@ class IMU
     IMU();
     void setup_register();
     void calibrateGyroscope();
+	void calibrateAcclerometer();
     void getRaw(long* ax, long* ay, long* az, int64_t* gx, int64_t* gy, int64_t* gz);
     void getGyroscope(int64_t* gyro_x, int64_t* gyro_y, int64_t* gyro_z);
     void getAcclerometer(long* acc_x, long* acc_y, long* acc_z);
